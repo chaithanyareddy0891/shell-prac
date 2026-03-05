02d3aecbc361879b169a7250a3e249c91f9ebe93
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2026-03-05 14:35:02 +0530

    05-variables

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "please enter your name: "
+read USERNAME
+echo "Hi $USERNAME"
+echo "please enter your password: "
+read -s PASSWORD
+echo "Thanks your are in now, you can proceed"
