016c3c61b0eaff7bdaaf0fa36a3bd274614b96f0
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2026-03-05 14:24:25 +0530

    adding variables

---
+++ 03-variables-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1="chaithanya"
+person2="sandhya"
+echo "$person1: Hi $person2, I heard there is an opening for a DevOps Engineer in your company. Is it still available?"
+echo "$person2: Yes $person1, there is a position open for a DevOps role with AWS experience."
+echo "$person1: That’s great! I have around 7.7 years of experience including AWS DevOps and SQL Server DBA."
+echo "$person2: That sounds good. Please share your resume and I will refer you to the hiring team."
+echo "$person1: Sure, I will send my resume today. Thanks for helping me with the referral."
+echo "$person2: You're welcome! Let me know once you apply so I can follow up with HR."
